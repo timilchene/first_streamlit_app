674337f85617b003c0b9899b61631e152f5e5045
Tim Ilchene <46544339+timilchene@users.noreply.github.com>
2022-10-21 12:07:47 +0100

    Added Header + Halloween Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Scareberry Moatmeal')
+streamlit.text('Kale, Spinach & Rocket Booooothie')
+streamlit.text('Hard-Boiled Scream-Range Egg')
