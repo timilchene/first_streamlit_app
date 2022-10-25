298ed1b58080357cc2fbc1344a34a9a07f91f464
Tim Ilchene <46544339+timilchene@users.noreply.github.com>
2022-10-25 17:14:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
+# imports
 import streamlit
 import pandas
-
 import requests
-
 import snowflake.connector
-
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
